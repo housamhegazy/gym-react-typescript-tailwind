eaa8cb6753b6bf1e3c5d0ac17ea36be220e04c96
Housam Hegazy <geohousam@gmail.com>
2023-04-23 11:33:14 +0000

    update

--- src/components/contactus/index.tsx
+++ src/components/contactus/index.tsx
@@ -10,11 +10,17 @@ const ContactUs = (props: Props) => {
         <Header>Contact Us</Header>
         <div className='lg:flex gap-8 justify-between items-center w-full'>
             <div className='basis-2/5 mb-5'>
-                <form className='flex flex-col w-full gap-4 px-2'>
+                <form 
+                action="https://formsubmit.co/geohousam2@gmail.com" 
+                method="POST"
+                // onSubmit={(e)=>{
+                //     e.preventDefault()
+                // }}
+                className='flex flex-col w-full gap-4 px-2'>
                     <input type='text'placeholder='name' className='bg-blue-300 rounded-full px-3 py-2 w-full placeholder:text-gray-500'/>
                     <input type="email" name=""placeholder='email' className='bg-blue-300 rounded-full px-3 py-2 w-full placeholder:text-gray-500' />
                     <textarea rows={5} cols={10} placeholder='message' className='bg-blue-300 rounded px-3 py-2 w-full placeholder:text-gray-500'/>
-                    <button className='bg-blue-900 text-white px-4 py-2 rounded mx-auto'>Send</button>
+                    <button type='submit' className='bg-blue-900 text-white px-4 py-2 rounded mx-auto'>Send</button>
                 </form>
             </div>
             <div className='basis-3/5'>
